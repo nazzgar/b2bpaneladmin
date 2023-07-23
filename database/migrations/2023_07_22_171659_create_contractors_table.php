3cf68982b006ba3418772513c66f4ab2304445d6
Mateusz Kozłowski <mateuszkozlowski0@gmail.com>
2023-07-23 20:03:35 +0200

    sth

---
+++ database/migrations/2023_07_22_171659_create_contractors_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contractors', function (Blueprint $table) {
+            $table->string('logo')->unique();
+            $table->string('nazwa');
+            $table->string('nip');
+            $table->string('regon');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contractors');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    dd(\B2BPanel\SharedModels\Contractor::all());
     return view('welcome');
 });
