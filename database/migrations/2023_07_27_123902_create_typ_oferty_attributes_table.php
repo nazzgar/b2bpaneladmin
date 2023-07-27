cbd6cabd3f7762228098948992f77b7ed8a879da
Mateusz Kozłowski <m.kozlowski@polanglo.pl>
2023-07-27 14:48:37 +0200

    create typ_oferty_attribute migration

---
+++ database/migrations/2023_07_27_123902_create_typ_oferty_attributes_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('typ_oferty_attributes', function (Blueprint $table) {
+            $table->integer('id')->unique();
+            $table->string('typoferty');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrentOnUpdate()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('typ_oferty_attributes');
+    }
+};
