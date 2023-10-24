4ecdce98b83e2d5e3cc893948ae179aaf6fc76fe
Mateusz Kozłowski <m.kozlowski@polanglo.pl>
2023-10-24 09:02:56 +0200

    filters

--- app/Filament/Resources/ReturnCampaignResource/RelationManagers/ContractorsRelationManager.php
+++ app/Filament/Resources/ReturnCampaignResource/RelationManagers/ContractorsRelationManager.php
@@ -6,6 +6,7 @@ use B2BPanel\SharedModels\Contractor;
 use B2BPanel\SharedModels\ReturnCampaign;
 use Filament\Forms;
 use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Resources\Table;
@@ -13,6 +14,8 @@ use Filament\Tables;
 use Filament\Tables\Actions\Action;
 use Filament\Tables\Actions\ActionGroup;
 use Filament\Tables\Actions\AttachAction;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\Layout;
 use Filament\Tables\Table as TablesTable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
@@ -50,8 +53,37 @@ class ContractorsRelationManager extends RelationManager
                 Tables\Columns\TextColumn::make('nip')
             ])
             ->filters([
-                //
-            ])
+                Filter::make('logo')
+                    ->form([
+                        TextInput::make('logo')->label('Logo')->debounce('1000ms')
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query->when(
+                            $data['logo'],
+                            fn (Builder $query, $logo): Builder => $query->where('logo', 'like', '%' . $logo . '%')
+                        );
+                    }),
+                Filter::make('nip')
+                    ->form([
+                        TextInput::make('nip')->label('Nip')->debounce('1000ms')
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query->when(
+                            $data['nip'],
+                            fn (Builder $query, $nip): Builder => $query->where('nip', 'like', '%' . $nip . '%')
+                        );
+                    }),
+                Filter::make('nazwa')
+                    ->form([
+                        TextInput::make('nazwa')->label('Nazwa')->debounce('1000ms')
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query->when(
+                            $data['nazwa'],
+                            fn (Builder $query, $nazwa): Builder => $query->where('nazwa', 'like', '%' . $nazwa . '%')
+                        );
+                    })
+            ], layout: Layout::AboveContent)
             ->headerActions([
                 AttachAction::make()->label('Dołącz jednego')->color('primary'),
                 Action::make('attach_all')->label('Dołącz wszystkich')->button()->requiresConfirmation()->action(function (ContractorsRelationManager $livewire) {
