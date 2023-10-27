0a702a6d18b5676ec6dcbcbf30401d0e7d45dfd6
Mateusz Kozłowski <m.kozlowski@polanglo.pl>
2023-10-27 09:36:00 +0200

    custom login page

---
+++ resources/views/livewire/login.blade.php
@@ -0,0 +1,23 @@
+<div class="space-y-4">
+
+    @if (session()->has('message'))
+
+    <div class="message message-success">
+
+        {{ session('message') }}
+
+    </div>
+
+    @endif
+
+
+    <form wire:submit.prevent="authenticate" class="space-y-8">
+        {{ $this->form }}
+
+        <x-filament::button type="submit" form="authenticate" class="w-full">
+            {{ __('filament::login.buttons.submit.label') }}
+        </x-filament::button>
+    </form>
+
+    <a href="{{route('forgot-password')}}" class="filament-link inline-flex items-center justify-center gap-0.5 font-medium outline-none hover:underline focus:underline text-primary-600 hover:text-primary-500 dark:text-primary-500 dark:hover:text-primary-400">Przypomnij hasło</a>
+</div>
